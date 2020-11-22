5b7675ba91255f1821d278f282c6718b552b266f
Dan Dumitriu <dandumitriu33@gmail.com>
2020-11-22 10:59:36 +0200

    Add DbContext and EFRepository

---
+++ Infrastructure/Data/AskMateContext.cs
@@ -0,0 +1,18 @@
+﻿using ApplicationCore.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Data
+{
+    public class AskMateContext : DbContext
+    {
+        public AskMateContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
---
+++ Infrastructure/Data/EFRepository.cs
@@ -0,0 +1,24 @@
+﻿using ApplicationCore.Entities;
+using ApplicationCore.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class EFRepository<T> : IAsyncRepository<T> where T : BaseEntity 
+    {
+        private readonly AskMateContext _dbContext;
+
+        public EFRepository(AskMateContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<List<T>> ListAllAsync()
+        {
+            return await _dbContext.Set<T>().ToListAsync();
+        }
+    }
+}
