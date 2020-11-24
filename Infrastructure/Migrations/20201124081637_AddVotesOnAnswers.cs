4c1614babdbba373a4ccfab057d097c5173bcd49
Dan Dumitriu <dandumitriu33@gmail.com>
2020-11-24 10:18:03 +0200

    Add Votes column on Answers

---
+++ Infrastructure/Migrations/20201124081637_AddVotesOnAnswers.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Migrations
+{
+    public partial class AddVotesOnAnswers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Votes",
+                table: "Answers",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Votes",
+                table: "Answers");
+        }
+    }
+}
