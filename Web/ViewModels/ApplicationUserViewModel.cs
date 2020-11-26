75b3de1178a507c6ef192dbe6ce907f5f5724015
Dan Dumitriu <dandumitriu33@gmail.com>
2020-11-26 19:07:43 +0200

    Add ApplicationUSerViewModel

---
+++ Web/ViewModels/ApplicationUserViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.ViewModels
+{
+    public class ApplicationUserViewModel
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public DateTime DateAdded { get; set; }
+    }
+}
