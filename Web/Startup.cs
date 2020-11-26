98bedbbcb9b0c94baa5c80addc7e4467e6bc8b22
Dan Dumitriu <dandumitriu33@gmail.com>
2020-11-26 13:33:42 +0200

    Add Startup Configure configuration for Authentication

--- Web/Startup.cs
+++ Web/Startup.cs
@@ -61,7 +61,7 @@ namespace Web
             app.UseStaticFiles();
 
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
