924925ddf8a2847071742c117c3567ad709d5378
Dan Dumitriu <dandumitriu33@gmail.com>
2020-11-27 18:29:33 +0200

    Add Comments view models UserId

--- Web/ViewModels/AnswerCommentViewModel.cs
+++ Web/ViewModels/AnswerCommentViewModel.cs
@@ -15,5 +15,6 @@ namespace Web.ViewModels
         public int AnswerId { get; set; }
         public DateTime DateAdded { get; set; }
         public bool IsEdited { get; set; }
+        public string UserId { get; set; }
     }
 }
