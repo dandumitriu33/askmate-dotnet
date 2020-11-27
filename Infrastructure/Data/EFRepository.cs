27ed3c5e1767170741b79f35962eb3aacccb29e9
Dan Dumitriu <dandumitriu33@gmail.com>
2020-11-27 20:30:25 +0200

    Add UserReputation incrase on vote up answer

--- Infrastructure/Data/EFRepository.cs
+++ Infrastructure/Data/EFRepository.cs
@@ -310,9 +310,11 @@ namespace Infrastructure.Data
             await using var transaction = await _dbContext.Database.BeginTransactionAsync();
             try
             {
+                int reputationModificationValue = 1;
                 var answerFromDb = await _dbContext.Answers.Where(a => a.Id == answerId && a.IsRemoved == false).FirstOrDefaultAsync();
 
                 answerFromDb.Votes += 1;
+                await ModifyUserReputation(reputationModificationValue, answerFromDb.UserId);
                 _dbContext.Answers.Attach(answerFromDb);
                 _dbContext.Entry(answerFromDb).State = EntityState.Modified;
                 await _dbContext.SaveChangesAsync();
