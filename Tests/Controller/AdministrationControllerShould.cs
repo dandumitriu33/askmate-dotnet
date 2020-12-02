0a798e54bef466b8a55be444f8f279630d241562
Dan Dumitriu <dandumitriu33@gmail.com>
2020-12-02 17:48:29 +0200

    Add Unit Test AdministrationController CreateRolePostInvalid model

--- Tests/Controller/AdministrationControllerShould.cs
+++ Tests/Controller/AdministrationControllerShould.cs
@@ -6,7 +6,9 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using Web.Controllers;
+using Web.ViewModels;
 using Xunit;
 
 namespace Tests.Controller
@@ -30,6 +32,22 @@ namespace Tests.Controller
 
             // Assert
             var viewResult = Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public async Task CreateRolePost_ReturnsViewResult_WhenModelStateIsInvalid()
+        {
+            // Arrange
+            var controller = new AdministrationController(roleManager, userManager, repository, mapper);
+            controller.ModelState.AddModelError("RoleName", "Required");
+            var newRoleViewModel = new RoleViewModel();
+
+            // Act
+            var result = await controller.CreateRole(newRoleViewModel);
+
+            // Assert
+            var badRequestResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsAssignableFrom<RoleViewModel>(badRequestResult.ViewData.Model);
         }
     }
 }
