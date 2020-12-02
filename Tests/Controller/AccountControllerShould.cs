54b82d898db7f21ac73a41cde992df42773aa19b
Dan Dumitriu <dandumitriu33@gmail.com>
2020-12-02 17:08:14 +0200

    Add Initial Tests for AccountController - Register

--- Tests/Controller/AccountControllerShould.cs
+++ Tests/Controller/AccountControllerShould.cs
@@ -1,10 +1,71 @@
-﻿using System;
+﻿using ApplicationCore.Entities;
+using ApplicationCore.Interfaces;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Web.Controllers;
+using Web.ViewModels;
+using Xunit;
 
 namespace Tests.Controller
 {
-    class AccountControllerShould
+    public class AccountControllerShould
     {
+        private UserManager<ApplicationUser> userManager { get; }
+        private SignInManager<ApplicationUser> signInManager { get; }
+
+        [Fact]
+        public void Register_ReturnAViewResult()
+        {
+            // Arrange
+            var controller = new AccountController(userManager, signInManager);
+
+            // Act
+            var result = controller.Register();
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public async Task RegisterPost_ReturnsViewResult_WhenModelStateIsInvalid()
+        {
+            // Arrange
+            var controller = new AccountController(userManager, signInManager);
+            controller.ModelState.AddModelError("Email", "Required");
+            var newRegisterViewModel = new RegisterViewModel();
+
+            // Act
+            var result = await controller.Register(newRegisterViewModel);
+
+            // Assert
+            var badRequestResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsAssignableFrom<RegisterViewModel>(badRequestResult.ViewData.Model);
+        }
+
+        [Fact]
+        public async Task RegisterPost_ReturnsViewResultError_WhenModelStateIsValidAndCreateFails()
+        {
+            // Arrange
+            var controller = new AccountController(userManager, signInManager);
+            var newRegisterViewModel = new RegisterViewModel();
+
+            // Act
+            var result = await controller.Register(newRegisterViewModel);
+
+            // Assert
+            var badRequestResult = Assert.IsType<ViewResult>(result);
+            Assert.Equal("Error", badRequestResult.ViewName);
+        }
+
+
+
     }
 }
