54c9b69f4f633d6c3909b306d55f80821a575c37
Dan Dumitriu <dandumitriu33@gmail.com>
2020-12-02 17:43:42 +0200

    Add Unit Test AdministrationController CreateRoleGet

---
+++ Tests/Controller/AdministrationControllerShould.cs
@@ -0,0 +1,35 @@
+﻿using ApplicationCore.Entities;
+using ApplicationCore.Interfaces;
+using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Web.Controllers;
+using Xunit;
+
+namespace Tests.Controller
+{
+    public class AdministrationControllerShould
+    {
+        private UserManager<ApplicationUser> userManager { get; }
+        private SignInManager<ApplicationUser> signInManager { get; }
+        private RoleManager<IdentityRole> roleManager { get; }
+        private IAsyncRepository repository { get; }
+        private IMapper mapper { get; }
+
+        [Fact]
+        public void CreateRoleGet_ReturnAViewResult()
+        {
+            // Arrange
+            var controller = new AdministrationController(roleManager, userManager, repository, mapper);
+
+            // Act
+            var result = controller.CreateRole();
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+        }
+    }
+}
