3cce24f7eae08b1f8e0c2b493028a549cb305c25
Dan Dumitriu <dandumitriu33@gmail.com>
2020-12-04 12:21:41 +0200

    Add Unit Test Administration AddUserToRolePost - EditUsersIRole when user already in role

--- Tests/Controller/AdministrationControllerShould.cs
+++ Tests/Controller/AdministrationControllerShould.cs
@@ -348,11 +348,56 @@ namespace Tests.Controller
             mockRoleManager.Verify(rm => rm.FindByIdAsync(It.IsAny<string>()), Times.Once);
             mockUserManager.Verify(um => um.FindByIdAsync(It.IsAny<string>()), Times.Once);
             mockUserManager.Verify(um => um.IsInRoleAsync(It.IsAny<ApplicationUser>(), It.IsAny<string>()), Times.Once);
-            mockUserManager.Verify(um => um.AddToRoleAsync(It.IsAny<ApplicationUser>(), It.IsAny<string>()), Times.Once);
-            
+            mockUserManager.Verify(um => um.AddToRoleAsync(It.IsAny<ApplicationUser>(), It.IsAny<string>()), Times.Once);            
         }
 
+        [Fact]
+        public async Task AddUserToRolePost_EditUsersInRoleViewOnAlreadyInRole()
+        {
+            // Arrange
 
+            // mocking UserManager            
+            var mockUserManager = MockHelpers.MockUserManager<ApplicationUser>();
+            ApplicationUser tempUser = new ApplicationUser { Email = "test@email.com" };
+            mockUserManager.Setup(um => um.FindByIdAsync(It.IsAny<string>())).ReturnsAsync(tempUser).Verifiable();
+            mockUserManager.Setup(um => um.IsInRoleAsync(It.IsAny<ApplicationUser>(), It.IsAny<string>())).ReturnsAsync(true).Verifiable();
+            mockUserManager.Setup(um => um.AddToRoleAsync(It.IsAny<ApplicationUser>(), It.IsAny<string>())).ReturnsAsync(IdentityResult.Success).Verifiable();
+
+            // mocking RoleManager
+            var mockRoleManager = MockHelpers.MockRoleManager<IdentityRole>();
+            IdentityRole tempRole = new IdentityRole() { Name = "Test Role" };
+            mockRoleManager.Setup(rm => rm.FindByIdAsync(It.IsAny<string>())).ReturnsAsync(tempRole).Verifiable();
+
+            // mocking Response.StatusCode = 404 setter
+            var mockHttpContext = new Mock<HttpContext>();
+            var response = new Mock<HttpResponse>();
+            mockHttpContext.SetupGet(x => x.Response).Returns(response.Object);
+
+            //creates an instance of an asp.net mvc controller
+            var controller = new AdministrationController(mockRoleManager.Object, mockUserManager.Object, repository, mapper)
+            {
+                ControllerContext = new ControllerContext()
+                {
+                    HttpContext = mockHttpContext.Object
+                }
+            };
+            UserRoleViewModel tempUserRoleViewModel = new UserRoleViewModel
+            {
+                RoleId = "abcd",
+                UserId = "efgh"
+            };
+
+            // Act
+            var result = await controller.AddUserToRole(tempUserRoleViewModel);
+
+            // Assert
+            var requestResult = Assert.IsType<ViewResult>(result);
+            Assert.Equal("EditUsersInRole", requestResult.ViewName);
+            mockRoleManager.Verify(rm => rm.FindByIdAsync(It.IsAny<string>()), Times.Once);
+            mockUserManager.Verify(um => um.FindByIdAsync(It.IsAny<string>()), Times.Once);
+            mockUserManager.Verify(um => um.IsInRoleAsync(It.IsAny<ApplicationUser>(), It.IsAny<string>()), Times.Once);
+            mockUserManager.Verify(um => um.AddToRoleAsync(It.IsAny<ApplicationUser>(), It.IsAny<string>()), Times.Never);
+        }
 
 
 
