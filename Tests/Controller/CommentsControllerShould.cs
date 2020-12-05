49ca773288b193c4c8f942aad8cae83a6df4f635
Dan Dumitriu <dandumitriu33@gmail.com>
2020-12-05 13:53:40 +0200

    Add Unit Test Comments RemoveQuestionComment r Error on null question

--- Tests/Controller/CommentsControllerShould.cs
+++ Tests/Controller/CommentsControllerShould.cs
@@ -1934,7 +1934,60 @@ namespace Tests.Controller
             mockRepo.Verify(x => x.RemoveQuestionCommentById(It.IsAny<int>()), Times.Never);
         }
 
+        [Fact]
+        public async Task RemoveQuestionComment_ReturnErrorViewOnNullQuestion()
+        {
+            // Arrange
+            // mocking repository
+            var mockRepo = new Mock<IAsyncRepository>();
+            QuestionComment tempQuestionComment = new QuestionComment { Id = 1, Body = "Test Body", UserId = "abcd" };
+            mockRepo.Setup(repo => repo.GetQuestionCommentById(It.IsAny<int>())).ReturnsAsync(tempQuestionComment).Verifiable();
+            Question tempQuestion = null;
+            mockRepo.Setup(repo => repo.GetQuestionByIdWithoutDetailsAsync(It.IsAny<int>())).ReturnsAsync(tempQuestion).Verifiable();
+            mockRepo.Setup(repo => repo.RemoveQuestionCommentById(It.IsAny<int>())).Verifiable();
 
+            // mock ClaimsPrincipal
+            // https://stackoverflow.com/questions/38557942/mocking-iprincipal-in-asp-net-core
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Name, "example name"),
+                new Claim(ClaimTypes.NameIdentifier, "abcd"),
+                new Claim("custom-claim", "example claim value"),
+            }, "mock"));
+
+            // mocking Response.StatusCode = 404 setter
+            var mockHttpContext = new Mock<HttpContext>();
+            var response = new Mock<HttpResponse>();
+            mockHttpContext.SetupGet(x => x.Response).Returns(response.Object);
+
+            // adding a real mapper
+            var myProfile = new AskMateProfiles();
+            var configuration = new MapperConfiguration(cfg => cfg.AddProfile(myProfile));
+            var realMapper = new Mapper(configuration);
+
+            //creates an instance of an asp.net mvc controller
+            var controller = new CommentsController(realMapper, mockRepo.Object, userManager)
+            {
+                ControllerContext = new ControllerContext()
+                {
+                    HttpContext = mockHttpContext.Object
+                }
+            };
+            controller.ControllerContext = new ControllerContext()
+            {
+                HttpContext = new DefaultHttpContext() { User = user }
+            };
+
+            // Act
+            var result = await controller.RemoveQuestionComment(1, 1);
+
+            // Assert
+            var requestResult = Assert.IsType<ViewResult>(result);
+            Assert.Equal("Error", requestResult.ViewName);
+            mockRepo.Verify(x => x.GetQuestionCommentById(It.IsAny<int>()), Times.Once);
+            mockRepo.Verify(x => x.GetQuestionByIdWithoutDetailsAsync(It.IsAny<int>()), Times.Once);
+            mockRepo.Verify(x => x.RemoveQuestionCommentById(It.IsAny<int>()), Times.Never);
+        }
 
 
 
