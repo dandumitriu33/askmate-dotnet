6bc1a466254a711bfedb3a0e102f7377c55a7213
Dan Dumitriu <dandumitriu33@gmail.com>
2020-12-05 14:49:00 +0200

    Add Unit Test HomeController Index r Error view on exception

--- Tests/Controller/HomeControllerShould.cs
+++ Tests/Controller/HomeControllerShould.cs
@@ -62,5 +62,50 @@ namespace Tests.Controller
             mockRepo.Verify(x => x.GetLatestQuestions(It.IsAny<int>()), Times.Once);
         }
 
+        [Fact]
+        public async Task IndexGet_ReturnErrorViewOnException()
+        {
+            // Arrange
+            // mocking repository
+            var mockRepo = new Mock<IAsyncRepository>();
+            Question tempQuestion = new Question { Id = 1, Title = "Test Title" };
+            Question tempQuestion2 = new Question { Id = 2, Title = "Test Title 2" };
+            List<Question> tempQuestionsList = new List<Question>();
+            tempQuestionsList.Add(tempQuestion);
+            tempQuestionsList.Add(tempQuestion2);
+            mockRepo.Setup(repo => repo.GetLatestQuestions(It.IsAny<int>())).Throws(new Exception()).Verifiable();
+
+            // adding a real mapper
+            var myProfile = new AskMateProfiles();
+            var configuration = new MapperConfiguration(cfg => cfg.AddProfile(myProfile));
+            var realMapper = new Mapper(configuration);
+
+            // mocking Response.StatusCode = 404 setter
+            var mockHttpContext = new Mock<HttpContext>();
+            var response = new Mock<HttpResponse>();
+            mockHttpContext.SetupGet(x => x.Response).Returns(response.Object);
+
+            //creates an instance of an asp.net mvc controller
+            var controller = new HomeController(logger, mockRepo.Object, realMapper)
+            {
+                ControllerContext = new ControllerContext()
+                {
+                    HttpContext = mockHttpContext.Object
+                }
+            };
+
+            // Act
+            var result = await controller.Index();
+
+            // Assert
+            var requestResult = Assert.IsType<ViewResult>(result);
+            Assert.Equal("Error", requestResult.ViewName);
+            mockRepo.Verify(x => x.GetLatestQuestions(It.IsAny<int>()), Times.Once);
+        }
+
+
+
+
+
     }
 }
