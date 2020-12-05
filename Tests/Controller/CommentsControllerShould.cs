d0c75ca02a9cb5cc6214c4cf3701d98ef127eb99
Dan Dumitriu <dandumitriu33@gmail.com>
2020-12-05 11:03:54 +0200

    Add Unit Test Comments AddQuestionCommentGet r Error on exception

--- Tests/Controller/CommentsControllerShould.cs
+++ Tests/Controller/CommentsControllerShould.cs
@@ -84,6 +84,40 @@ namespace Tests.Controller
             mockRepo.Verify(x => x.GetQuestionByIdWithoutDetailsAsync(It.IsAny<int>()), Times.Once);
         }
 
+        [Fact]
+        public async Task AddQuestionCommentGet_ReturnViewErrorOnException()
+        {
+            // Arrange
+            // mocking repository
+            var mockRepo = new Mock<IAsyncRepository>();
+            Question tempQuestion = new Question { Id = 1, Title = "Test Title" };
+            mockRepo.Setup(repo => repo.GetQuestionByIdWithoutDetailsAsync(It.IsAny<int>())).Throws(new Exception()).Verifiable();
+
+            // mocking Response.StatusCode = 404 setter
+            var mockHttpContext = new Mock<HttpContext>();
+            var response = new Mock<HttpResponse>();
+            mockHttpContext.SetupGet(x => x.Response).Returns(response.Object);
+
+            //creates an instance of an asp.net mvc controller
+            var controller = new CommentsController(mapper, mockRepo.Object, userManager)
+            {
+                ControllerContext = new ControllerContext()
+                {
+                    HttpContext = mockHttpContext.Object
+                }
+            };
+
+            // Act
+            var result = await controller.AddQuestionComment(1);
+
+            // Assert
+            var requestResult = Assert.IsType<ViewResult>(result);
+            Assert.Equal("Error", requestResult.ViewName);
+            mockRepo.Verify(x => x.GetQuestionByIdWithoutDetailsAsync(It.IsAny<int>()), Times.Once);
+        }
+
+
+
 
     }
 }
