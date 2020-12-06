f7bd9868620187924dc93010c8035fc9eb05b245
Dan Dumitriu <dandumitriu33@gmail.com>
2020-12-06 12:19:56 +0200

    Add Unit Test Tags AttachTagPost r Deatils Action on success new tag

--- Tests/Controller/TagsControllerShould.cs
+++ Tests/Controller/TagsControllerShould.cs
@@ -101,7 +101,7 @@ namespace Tests.Controller
         }
 
         [Fact]
-        public async Task AttachTagPost_RedirectToDetailsActionOnSuccess()
+        public async Task AttachTagPost_RedirectToDetailsActionOnSuccessExistingTag()
         {
             // Arrange
             // mocking repository
@@ -133,7 +133,38 @@ namespace Tests.Controller
             mockRepo.Verify(x => x.AddQuestionTagAsync(It.IsAny<QuestionTag>()), Times.Once);
         }
 
+        [Fact]
+        public async Task AttachTagPost_RedirectToDetailsActionOnSuccessNewTag()
+        {
+            // Arrange
+            // mocking repository
+            var mockRepo = new Mock<IAsyncRepository>();
+            Question tempQuestion = new Question { Id = 1, Title = "Test Title" };
+            mockRepo.Setup(repo => repo.GetQuestionByIdWithoutDetailsAsync(It.IsAny<int>())).ReturnsAsync(tempQuestion).Verifiable();
+            Tag tempTag = new Tag { Id = 1, Name = "Test Tag" };
+            Tag tempNullTag = null;
+            mockRepo.SetupSequence(repo => repo.GetTagByIdAsync(It.IsAny<int>())).ReturnsAsync(tempNullTag).ReturnsAsync(tempTag);
+            mockRepo.Setup(repo => repo.AddTagAsync(It.IsAny<Tag>())).ReturnsAsync(tempTag).Verifiable();
+            mockRepo.Setup(repo => repo.AddQuestionTagAsync(It.IsAny<QuestionTag>())).Verifiable();
 
+            // adding a real mapper
+            var myProfile = new AskMateProfiles();
+            var configuration = new MapperConfiguration(cfg => cfg.AddProfile(myProfile));
+            var realMapper = new Mapper(configuration);
+
+            var controller = new TagsController(mockRepo.Object, realMapper);
+
+            // Act
+            var result = await controller.AttachTag(new TagViewModel { Id = 1, Name = "Test Name" }, 1);
+
+            // Assert
+            var requestResult = Assert.IsType<RedirectToActionResult>(result);
+            Assert.Equal("Details", requestResult.ActionName);
+            Assert.Equal("Questions", requestResult.ControllerName);
+            mockRepo.Verify(x => x.GetQuestionByIdWithoutDetailsAsync(It.IsAny<int>()), Times.Exactly(2));
+            mockRepo.Verify(x => x.AddTagAsync(It.IsAny<Tag>()), Times.Once);
+            mockRepo.Verify(x => x.AddQuestionTagAsync(It.IsAny<QuestionTag>()), Times.Once);
+        }
 
 
 
