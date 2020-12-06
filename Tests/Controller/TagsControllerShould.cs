d71971d2938465716894fef5492b313493c3f466
Dan Dumitriu <dandumitriu33@gmail.com>
2020-12-06 11:13:56 +0200

    Add Unit Test Tags AttachTagGet r Error on null question

--- Tests/Controller/TagsControllerShould.cs
+++ Tests/Controller/TagsControllerShould.cs
@@ -45,6 +45,42 @@ namespace Tests.Controller
             mockRepo.Verify(x => x.GetAllTagsNoDuplicates(It.IsAny<int>()), Times.Once);
         }
 
+        [Fact]
+        public async Task AttachTagGet_ReturnErrorViewOnNullQuestion()
+        {
+            // Arrange
+            // mocking repository
+            var mockRepo = new Mock<IAsyncRepository>();
+            Question tempQuestion = null;
+            mockRepo.Setup(repo => repo.GetQuestionByIdWithoutDetailsAsync(It.IsAny<int>())).ReturnsAsync(tempQuestion).Verifiable();
+            mockRepo.Setup(repo => repo.GetAllTagsNoDuplicates(It.IsAny<int>())).ReturnsAsync(new List<Tag>()).Verifiable();
+
+            // adding a real mapper
+            var myProfile = new AskMateProfiles();
+            var configuration = new MapperConfiguration(cfg => cfg.AddProfile(myProfile));
+            var realMapper = new Mapper(configuration);
+
+            var controller = new TagsController(mockRepo.Object, realMapper);
+
+            // Act
+            var result = await controller.AttachTag(1);
+
+            // Assert
+            var requestResult = Assert.IsType<ViewResult>(result);
+            Assert.Equal("Error", requestResult.ViewName);
+            mockRepo.Verify(x => x.GetQuestionByIdWithoutDetailsAsync(It.IsAny<int>()), Times.Once);
+            mockRepo.Verify(x => x.GetAllTagsNoDuplicates(It.IsAny<int>()), Times.Never);
+        }
+
+
+
+
+
+
+
+
+
+
 
     }
 }
