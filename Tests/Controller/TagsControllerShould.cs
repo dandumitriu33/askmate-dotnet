6e51d93bfb55ae7f7573567b6b005a0ab775e95d
Dan Dumitriu <dandumitriu33@gmail.com>
2020-12-06 14:05:23 +0200

    Add Unit Test Tags AttachQuestionTag r Error view on exception

--- Tests/Controller/TagsControllerShould.cs
+++ Tests/Controller/TagsControllerShould.cs
@@ -577,7 +577,46 @@ namespace Tests.Controller
             mockRepo.Verify(x => x.AttachQuestionTagAsync(It.IsAny<QuestionTag>()), Times.Never);
         }
 
+        [Fact]
+        public async Task AttachQuestionTagGet_ReturnErrorViewOnException()
+        {
+            // Arrange
+            // mocking repository
+            var mockRepo = new Mock<IAsyncRepository>();
+            Question tempQuestion = new Question { Id = 1, Title = "Test Title" };
+            mockRepo.Setup(repo => repo.GetQuestionByIdWithoutDetailsAsync(It.IsAny<int>())).ReturnsAsync(tempQuestion).Verifiable();
+            Tag tempTag = new Tag { Id = 1, Name = "Test Tag" };
+            mockRepo.Setup(repo => repo.GetTagByIdAsync(It.IsAny<int>())).ReturnsAsync(tempTag).Verifiable();
+            mockRepo.Setup(repo => repo.AttachQuestionTagAsync(It.IsAny<QuestionTag>())).Throws(new Exception()).Verifiable();
 
+            // adding a real mapper
+            var myProfile = new AskMateProfiles();
+            var configuration = new MapperConfiguration(cfg => cfg.AddProfile(myProfile));
+            var realMapper = new Mapper(configuration);
+
+            // mocking Response.StatusCode = 404 setter
+            var mockHttpContext = new Mock<HttpContext>();
+            var response = new Mock<HttpResponse>();
+            mockHttpContext.SetupGet(x => x.Response).Returns(response.Object);
+
+            var controller = new TagsController(mockRepo.Object, realMapper)
+            {
+                ControllerContext = new ControllerContext()
+                {
+                    HttpContext = mockHttpContext.Object
+                }
+            };
+
+            // Act
+            var result = await controller.AttachQuestionTag(1, 1);
+
+            // Assert
+            var requestResult = Assert.IsType<ViewResult>(result);
+            Assert.Equal("Error", requestResult.ViewName);
+            mockRepo.Verify(x => x.GetQuestionByIdWithoutDetailsAsync(It.IsAny<int>()), Times.Once);
+            mockRepo.Verify(x => x.GetTagByIdAsync(It.IsAny<int>()), Times.Once);
+            mockRepo.Verify(x => x.AttachQuestionTagAsync(It.IsAny<QuestionTag>()), Times.Once);
+        }
 
 
 
